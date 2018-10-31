758a5409540b609108c622a9c3f5139ceeddcc11
Matteo Hoch <minime@hochweb.com>
2018-10-31 16:38:41 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,8 +7,8 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
 #install anaconda without prompt
 wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh -O Anaconda3-Linux-x86_64.sh
 bash Anaconda3-Linux-x86_64.sh -b -p ~/anaconda
-echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
-echo 'export PYTHONHOME="~/anaconda/bin"' >> ~/.bashrc
+echo 'export PATH="~/anaconda/bin/:$PATH"' >> ~/.bashrc
+echo 'export PYTHONHOME="~/anaconda/bin/"' >> ~/.bashrc
 
 fuser 5001/tcp
 fuser -k 5001/tcp #kill all proc on port
