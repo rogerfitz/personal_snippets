f2d308a5bfe24c1283d1b806d5a28ac92dc4c044
Matteo Hoch <minime@hochweb.com>
2019-02-06 19:26:51 -0600

    updated conda version

--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@ docker run -it -p "127.0.0.1:8081:8080"
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
 
 #install anaconda without prompt
-wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh -O Anaconda3-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh -O Anaconda3-Linux-x86_64.sh
 bash Anaconda3-Linux-x86_64.sh -b -p ~/anaconda
 echo 'export PATH="~/anaconda/bin/:$PATH"' >> ~/.bashrc
 echo 'export PYTHONHOME="~/anaconda/bin/"' >> ~/.bashrc
