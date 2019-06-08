a78010bc6aee75fda46eae66f78eb161a5e6706a
Matteo Hoch <minime@hochweb.com>
2019-06-08 10:06:57 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,5 @@
 #dynamic path for common projects
 import os,sys
 
-folder_name="PROJECT_NAME"
+folder_name="PROJECT_NAME"#must be unique on the abspath
 BASE_DIR=os.path.abspath(".").split(folder_name)[0]+folder_name
